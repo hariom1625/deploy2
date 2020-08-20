8329d382175688b9ed53c826e6f7e7da0d5347a2
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2020-08-21 03:20:54 +0530

    initial commit

---
+++ src/components/Main/Footer/DevelopersList.js
@@ -0,0 +1,65 @@
+export const DEVELOPERS =
+    [
+      {
+        id: 0,
+        name:'Yash Pandey',
+        intro: 'The GitHub Guy. Opensource Developer. ',
+        img:'/assets/images/yash.jpeg',
+        mutedText : 'MERN stack web developer',
+        linkedin: 'https://www.linkedin.com/in/yash-p-1793b185/',
+        github:'https://github.com/EmperorYP7',
+        mail:'yash.btech.cs19@iiitranchi.ac.in'
+       },
+       {
+        id: 1,
+        name:'Hari Om Yadav',
+        intro: ' essentially unchanged',
+        mutedText : 'Someone famous in',
+        img:'/assets/images/hariom.jpeg',
+        linkedin: 'https://',
+        github:'https://',
+        mail:'https://'
+       },
+      {
+        id: 2,
+        name:'Tushar Jain',
+        intro: 'It has survived not only five centuries,  unchanged',
+        mutedText : 'MERN stack web developer',
+        img:'/assets/images/tushar.png',
+        linkedin: 'https://www.linkedin.com/in/tushar-jain-4b2ab219b/',
+        github:'https://github.com/tusharjain0022',
+        mail:'tusharjain.btech.cs19@iiitranchi.ac.in'
+       },
+      {
+        id: 3,
+        name:'Rishav Mazumdar',
+        intro: 'Web Developer , Python Enthusiast.',
+        img:'/assets/images/rishabh.jpeg',
+        linkedin: 'https://in.linkedin.com/in/rishav-mazumdar-38b869192/',
+        github:'https://github.com/RishavMz',
+        mail:'rishav.btech.ec19@iiitranchi.ac.in'
+      },
+
+       {
+        id: 4,
+        name:'Ashish Chaurasia',
+        intro: 'Will update soon',
+        img:'assets/images/ashish.jpg',
+        mutedText : 'MERN stack web developer (Specialised in backend)',
+        linkedin: 'https://www.linkedin.com/in/ashish-chourasia-943a06191/',
+        github:'https://github.com/sleepysleep/',
+        mail:'ashish.btech.cs19@iiitranhi.ac.in'
+       },
+       {
+        id: 5,
+        name:'Shubham Kushwaha',
+        intro: '',
+        img:'',
+        mutedText : 'Someone famous in',
+        linkedin: 'https://',
+        github:'https://',
+        mail:'https://'
+       }
+
+
+    ];
