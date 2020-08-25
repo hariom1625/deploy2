985cd2a1582327d62a24713ca04b0167d45b2ffe
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2020-08-25 19:05:07 +0530

    society card

--- src/components/Main/Events/Event.js
+++ src/components/Main/Events/Event.js
@@ -69,6 +69,10 @@ class Event extends Component {
         })
     }
     render() {
+      const url = document.URL;
+
+      console.log(url);
+
         if (this.state.loaded) {
         return (
                 <div className=" container about-event">
